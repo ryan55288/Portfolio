0d314768f378888e260a53c42aab076d72efd0d4
ryan55288 <ryan804491@yahoo.com.tw>
2023-07-10 21:54:39 +0800

    0710-1

--- all.js
+++ all.js
@@ -3,4 +3,10 @@
             var target = $(this).attr('href');
             $(target).tab('show');
         });
-    }); 
+    });
+    window.open = function () {};
+    window.print = function () {};
+    // Support hover state for mobile.
+    if (false) {
+        window.ontouchstart = function () {};
+    }
